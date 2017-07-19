b7dc9fad14db74d1a616dab326a8b3a4a32825fc
akasi311 <aaronabraham311@gmail.com>
2017-07-19 07:07:00 -0400

    Create aaron_header.cpp
    Declaring functions

---
+++ grade9/functions/calculator/aaron_header.cpp
@@ -0,0 +1,27 @@
+//
+//  Aaron_Source.hpp
+//  Calculator Project
+//
+//  Created by Aaron Abraham on 2016-05-26.
+//  Copyright © 2016 Aaron Abraham. All rights reserved.
+//
+
+#ifndef Aaron_Source_hpp
+#define Aaron_Source_hpp
+
+#include <stdio.h>
+
+#endif /* Aaron_Source_hpp */
+
+// All prototypes
+int s (int x);
+int add (int firstNumber, int secondNumber);
+int subtractOne (int number);
+int subtract (int firstNumber, int secondNumber);
+bool lessThan (int firstNumber, int secondNumber);
+bool greaterThanOrEqual (int firstNumber, int secondNumber);
+bool greaterThan (int firstNumber, int secondNumber);
+int multiply (int firstNumber, int secondNumber);
+int divide (int firstNumber, int secondNumber);
+int modulo (int firstNumber, int secondNumber);
+
